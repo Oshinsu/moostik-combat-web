3c27c8190e31a6a79d9170364de4b7e029f5c623
Gary Byss <garybyss972@gmail.com>
2026-06-25 08:23:59 -0400

    FIX online : detection desync non-fatale (ne coupe plus la partie)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782345270|9631695';
+const CACHE_VERSION = '1782390236|10651543';
 /** @type {string} */
 const CACHE_PREFIX = 'Moostik Combat-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
